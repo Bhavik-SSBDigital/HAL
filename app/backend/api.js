5268b13e9e15c20c7966cb740ef4248d8fe09c1d
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2024-10-17 10:55:14 +0530

    Update api.js

--- app/backend/api.js
+++ app/backend/api.js
@@ -31,7 +31,12 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/dms.ssbd.in/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/dms.ssbd.in/fullchain.pem"),
+};
+
+const server = https.createServer(options, app);
 
 const io = new Server(server, {
   cors: {
