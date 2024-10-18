52d5ecbc3e9f1947cd20510e51ff80eb5f917243
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2024-10-18 19:53:53 +0530

    restricting transports to websocket connection only for io server

--- app/backend/api.js
+++ app/backend/api.js
@@ -40,6 +40,7 @@ const server = https.createServer(options, app);
 
 const io = new Server(server, {
   // path: "/socket/",
+  transports: ["websocket"],
   cors: {
     origin: "*",
     methods: ["GET", "POST"], // You can specify other allowed methods as needed
