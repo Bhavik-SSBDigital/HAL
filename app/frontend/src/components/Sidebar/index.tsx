c468fc5fc867a6a73b7ebfb549fae5c9ff5d92f7
virajkalaria <viraj.kalariya@ssbi.in>
2024-11-08 10:34:35 +0530

    update

--- app/frontend/src/components/Sidebar/index.tsx
+++ app/frontend/src/components/Sidebar/index.tsx
@@ -100,7 +100,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
           strokeLinecap="round"
           color="white"
           strokeLinejoin="round"
--- app/frontend/src/pages/Authentication/SignIn.tsx
+++ app/frontend/src/pages/Authentication/SignIn.tsx
@@ -253,7 +253,7 @@ const SignIn: React.FC = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
+                        strokeWidth="2"
                         strokeLinecap="round"
                         strokeLinejoin="round"
                       >
