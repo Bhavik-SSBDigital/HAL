604f6324a7e3690479006fdb9e00fc36308db523
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2024-11-15 16:15:22 +0530

    build for cloud server

--- app/backend/api.js
+++ app/backend/api.js
@@ -63,7 +63,7 @@ export const userSockets = new Map();
 //   cert: await fs.readFile('./certificate.pem')
 // };
 
-server.listen(8000, () => console.log(`Listening on port ${8000}`));
+server.listen(5000, () => console.log(`Listening on port ${5000}`));
 
 server.on("error", (error) => {
   console.error("Server error:", error);
