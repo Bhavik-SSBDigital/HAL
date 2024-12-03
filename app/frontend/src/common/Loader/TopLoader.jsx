345617d05d1b74556cf18320b7a0a402738d6164
virajkalaria <viraj.kalariya@ssbi.in>
2024-12-03 16:31:06 +0530

    top loader

---
+++ app/frontend/src/common/Loader/TopLoader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './TopLoader.module.css';
+
+const TopLoader = () => {
+  return <div className={styles.topLoader}></div>;
+};
+
+export default TopLoader;
