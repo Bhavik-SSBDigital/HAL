2de38aecdd93e7e5967e76df06b2b5e9cb4fd726
virajkalaria <viraj.kalariya@ssbi.in>
2025-01-08 16:12:13 +0530

    build bug solve

--- app/Frontend/vite.config.js
+++ app/Frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window', // Polyfill global as window in the browser
-  },
   server: {
     port: 3000, // Change this to the desired port number
   },
