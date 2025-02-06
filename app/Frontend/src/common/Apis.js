7c54e13c6f1b8cb630a34bbadc66598cb98dc4a3
virajkalaria <viraj.kalariya@ssbi.in>
2025-02-06 11:23:47 +0530

    Api file

---
+++ app/Frontend/src/common/Apis.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const getAccessToken = () => sessionStorage.getItem('accessToken');
+
+const apiClient = axios.create({
+  baseURL: backendUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use((config) => {
+  const token = getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// department endpoints
+export const getDepartments = async () => {
+  return apiClient.get('/getdepartments', { params: { type: 'department' } });
+};
+
+// users endpoints
+export const getUsers = async () => {
+  return apiClient.get('/getUsers', { params: { isRootUser: true } });
+};
