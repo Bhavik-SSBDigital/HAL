3840666fe5d64c99b9914cd304d1b461099f4f29
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2025-03-17 17:12:22 +0530

    update in schema

---
+++ app/backend-postgres/prisma/migrations/20250317114207_removed_unnecessaryproperty_from_process_step_instance/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `assignedType` on the `ProcessStepInstance` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProcessStepInstance" DROP COLUMN "assignedType";
