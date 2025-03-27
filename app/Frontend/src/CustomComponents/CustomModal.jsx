29517d2db448852344f8a7c0af96c56aceb63ec6
virajkalaria <viraj.kalariya@ssbi.in>
2025-03-27 13:07:03 +0530

    Update CustomModal.jsx

--- app/Frontend/src/CustomComponents/CustomModal.jsx
+++ app/Frontend/src/CustomComponents/CustomModal.jsx
@@ -6,12 +6,6 @@ export default function CustomModal({ children, isOpen, onClose }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
-        <button
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-          onClick={onClose}
-        >
-          &times;
-        </button>
         {children}
       </div>
     </div>
