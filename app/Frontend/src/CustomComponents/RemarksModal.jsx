e6752e79e429796b2cc3e96d8665ce31ace6c1bd
virajkalaria <viraj.kalariya@ssbi.in>
2025-04-16 12:46:11 +0530

    UI Update

--- app/Frontend/src/CustomComponents/RemarksModal.jsx
+++ app/Frontend/src/CustomComponents/RemarksModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import CustomModal from './CustomModal';
 import CustomButton from './CustomButton';
 
 const RemarksModal = ({ open, title, onSubmit, onClose, loading }) => {
@@ -10,8 +11,6 @@ const RemarksModal = ({ open, title, onSubmit, onClose, loading }) => {
     }
   }, [open]);
 
-  if (!open) return null;
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (remark.trim()) {
@@ -20,30 +19,29 @@ const RemarksModal = ({ open, title, onSubmit, onClose, loading }) => {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <h2 className="text-lg font-semibold mb-4">{title}</h2>
-        <form onSubmit={handleSubmit}>
-          <textarea
-            className="w-full p-2 border rounded-lg focus:outline-none"
-            rows="4"
-            value={remark}
-            onChange={(e) => setRemark(e.target.value)}
-            placeholder="Enter your remarks here..."
-            required
+    <CustomModal isOpen={open} onClose={onClose} className="w-96">
+      <h2 className="text-lg font-semibold mb-4">{title}</h2>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className="w-full p-2 border rounded-lg focus:outline-none"
+          rows="4"
+          value={remark}
+          onChange={(e) => setRemark(e.target.value)}
+          placeholder="Enter your remarks here..."
+          required
+        />
+        <div className="flex justify-end mt-4 space-x-2">
+          <CustomButton
+            disabled={loading}
+            type={'button'}
+            text="Cancel"
+            variant="danger"
+            click={onClose}
           />
-          <div className="flex justify-end mt-4 space-x-2">
-            <CustomButton
-              disabled={loading}
-              text={'Cancel'}
-              variant={'danger'}
-              click={onClose}
-            />
-            <CustomButton disabled={loading} text={'Submit'} type={'submit'} />
-          </div>
-        </form>
-      </div>
-    </div>
+          <CustomButton disabled={loading} text="Submit" type="submit" />
+        </div>
+      </form>
+    </CustomModal>
   );
 };
 
