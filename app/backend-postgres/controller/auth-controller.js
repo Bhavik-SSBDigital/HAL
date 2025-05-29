660a831fd529a9687b051eab868bcb2f6164425a
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2025-05-29 12:21:16 +0530

    Update auth-controller.js

--- app/backend-postgres/controller/auth-controller.js
+++ app/backend-postgres/controller/auth-controller.js
@@ -106,7 +106,6 @@ export const sign_up = async (req, res) => {
     res.status(200).json({ message: "User created successfully" });
   } catch (error) {
     console.error("Error creating user", error);
-    await prisma.user.delete({ where: { id: user.id } });
     return res.status(500).json({ message: "Error creating user" });
   }
 };
