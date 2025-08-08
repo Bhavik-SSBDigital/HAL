4ab56e5271e9db59b22faf1a5b0f7e5372368742
virajkalaria <viraj.kalariya@ssbi.in>
2025-08-08 12:43:15 +0530

    Update View.jsx

--- app/frontend/src/pages/view/View.jsx
+++ app/frontend/src/pages/view/View.jsx
@@ -156,7 +156,7 @@ const PdfViewer = ({
       isOpen={!!docu}
       onClose={handleViewClose}
       className="max-h-[90vh] overflow-auto"
-      size='full'
+      size={'2xl'}
     >
       <div className="sticky top-0 left-0 right-0 z-50 flex items-center justify-between px-4 py-2 bg-white shadow-md border-b rounded-t-lg">
         <div className="flex items-center gap-2">
