320ced3ec09e92d2578d611df8cc9d95d8663e0a
virajkalaria <viraj.kalariya@ssbi.in>
2025-08-29 17:28:19 +0530

    Update View.jsx

--- app/frontend/src/pages/view/View.jsx
+++ app/frontend/src/pages/view/View.jsx
@@ -158,7 +158,7 @@ const PdfViewer = ({
       className="max-h-[90vh] overflow-auto"
       size={'full'}
     >
-      <div className="sticky top-0 left-0 right-0 z-50 flex items-center justify-between px-4 py-2 bg-white shadow-md border-b rounded-t-lg">
+      <div className="sticky top-0 left-0 right-0 z-50 flex items-center justify-between px-4 py-2 bg-white border rounded-t-lg">
         <div className="flex items-center gap-2">
           {documents.length > 1 && (
             <>
